d8fe2db8e1b780cfee3877637dca6c5d6b700bd2
shanag1801 <shanag0110@gmail.com>
2024-04-14 19:18:11 +0300

    design

---
+++ app/src/main/java/com/example/solvequadratic/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.solvequadratic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void go1(View view) {
+    }
+
+    public void go2(View view) {
+    }
+
+    public void go3(View view) {
+    }
+
+    public void solve(View view) {
+    }
+}
