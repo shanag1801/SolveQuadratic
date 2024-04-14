b51d12f30f1cf1981e013f3b1136e28af3eb7690
shanag1801 <shanag0110@gmail.com>
2024-04-15 01:00:51 +0300

    pass solutions back to MainActivity

--- app/src/main/java/com/example/solvequadratic/MainActivity.java
+++ app/src/main/java/com/example/solvequadratic/MainActivity.java
@@ -8,24 +8,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     EditText et1, et2, et3;
     Button btn, btn2;
+    TextView tv10;
     int a, b, c;
     Random rnd = new Random();
+    Intent si;
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint({"MissingInflatedId", "SetTextI18n"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        et1 = (EditText) findViewById(R.id.et1);
-        et2 = (EditText) findViewById(R.id.et2);
-        et3 = (EditText) findViewById(R.id.et3);
-        btn = (Button) findViewById(R.id.btn);
-        btn2 = (Button) findViewById(R.id.btn2);
+        et1 = findViewById(R.id.et1);
+        et2 = findViewById(R.id.et2);
+        et3 = findViewById(R.id.et3);
+        btn = findViewById(R.id.btn);
+        btn2 = findViewById(R.id.btn2);
+        tv10 = findViewById(R.id.textView10);
+        si = getIntent();
+        double root1 = si.getDoubleExtra("root1", 1);
+        double root2 = si.getDoubleExtra("root2", 1);
+        tv10.setText("The solution/s are: " + root1 + ", " + root2);
+
     }
 
     public int a() {
@@ -44,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         a = a();
         b = b();
         c = c();
-        Intent si = new Intent(this, MainActivity.class);
+        si = new Intent(this, MainActivity.class);
         si.putExtra("a", a);
         si.putExtra("b", b);
         si.putExtra("c", c);
--- app/src/main/java/com/example/solvequadratic/MainActivity2.java
+++ app/src/main/java/com/example/solvequadratic/MainActivity2.java
@@ -11,31 +11,32 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity2 extends AppCompatActivity {
     TextView tv1, tv2;
     Button btn3;
+    Intent gi;
+    double root1, root2;
 
 
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        tv1 = (TextView) findViewById(R.id.textView5);
-        tv2 = (TextView) findViewById(R.id.textView6);
-        btn3 = (Button) findViewById(R.id.button);
-        Intent gi = new Intent();
+        tv1 = findViewById(R.id.textView5);
+        tv2 = findViewById(R.id.textView6);
+        btn3 =  findViewById(R.id.button);
         gi = getIntent();
         int a = gi.getIntExtra("a", 0);
         int b = gi.getIntExtra("b", 0);
         int c = gi.getIntExtra("c", 0);
+        finish();
 
         double discriminant = b * b - 4 * a * c;
-
         if (discriminant > 0) {
-            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
             tv1.setText(String.valueOf(root1));
             tv2.setText(String.valueOf(root2));
         } else if (discriminant == 0) {
-            double root = -b / (2 * a);
-            tv1.setText(String.valueOf(root));
+            root1 = -b / (2 * a);
+            tv1.setText(String.valueOf(root1));
             tv2.setText("No solution");
         } else {
             tv1.setText("No solution");
@@ -45,6 +46,10 @@ public class MainActivity2 extends AppCompatActivity {
     }
 
     public void back(View view) {
+        gi.putExtra("root1", root1);
+        gi.putExtra("root2", root2);
+        setResult(1, gi);
+        finish();
     }
 
 }
